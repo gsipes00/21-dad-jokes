3848968f824dd989e7275935c25fdb5180dcd28e
Gene Sipes <gsipes00@yahoo.com>
2022-08-03 14:44:20 -0500

    dad jokes project updates

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Dad Jokes Starter");
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+const url = "https://icanhazdadjoke.com/";
+
+btn.addEventListener("click", () => {
+  fetchDadJoke();
+});
+
+const fetchDadJoke = async () => {
+  const response = await fetch(url, {
+    headers: {
+      Accept: "application/json",
+      "User-Agent": "Gene is Learning",
+    },
+  });
+  const data = await response.json();
+  result.textContent = `${data.joke} ID: ${data.id}`;
+};
+
+fetchDadJoke();
